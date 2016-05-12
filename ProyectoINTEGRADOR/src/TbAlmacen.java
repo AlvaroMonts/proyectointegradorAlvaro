d4a36c6b252a12c836c9e707416ad4b8efb111a9
alvaro.montes <alvaro.montes@WKSALMPBB437.u-tad.es>
2016-05-12 16:00:45 +0200

    unido

--- ProyectoINTEGRADOR/src/TbAlmacen.java
+++ ProyectoINTEGRADOR/src/TbAlmacen.java
@@ -5,10 +5,43 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JToolBar;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TbAlmacen extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JLabel lblTipodeequipo;
+	private JLabel lblCausa;
+	private JLabel lblNotas;
+	private JLabel lblPiezareutilizable;
+	private JButton button_2;
+	private JButton button_4;
+	private JButton btnonEquipo;
+	private JButton btnHistorial;
+	private JButton btnPrestamos;
+	private JButton btnUsuario;
+	private JToolBar toolBar;
+	private TbEquipos Equipos;
+	private TbHistorial Historial;
+	private TbPrestamos Prestamos;
+	private TbUsers Users;
 
 	/**
 	 * Launch the application.
@@ -31,12 +64,365 @@ public class TbAlmacen extends JFrame {
 	 */
 	public TbAlmacen() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 819, 478);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(135, 206, 235));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setToolTipText("");
+
+		toolBar = new JToolBar();
+
+		button_2 = new JButton("Buscar");
+
+		button_4 = new JButton("Quitar filtro\r\n");
+
+		lblTipodeequipo = new JLabel("Tipo de Equipo");
+
+		textField = new JTextField();
+		textField.setColumns(10);
+
+		lblNotas = new JLabel("Notas\r\n");
+
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+
+		JLabel lblCodequipo = new JLabel("Codigo del equipo\r\n");
+
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+
+		lblCausa = new JLabel("Causa");
+
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+
+		lblPiezareutilizable = new JLabel("Piezas Reutilizables");
+
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane
+				.setHorizontalGroup(gl_contentPane
+						.createParallelGroup(Alignment.TRAILING)
+						.addGroup(
+								gl_contentPane
+										.createSequentialGroup()
+										.addContainerGap()
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.TRAILING)
+														.addGroup(
+																Alignment.LEADING,
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addGroup(
+																				gl_contentPane
+																						.createParallelGroup(
+																								Alignment.LEADING)
+																						.addGroup(
+																								gl_contentPane
+																										.createSequentialGroup()
+																										.addComponent(
+																												button_2)
+																										.addPreferredGap(
+																												ComponentPlacement.UNRELATED)
+																										.addComponent(
+																												button_4))
+																						.addComponent(
+																								textField,
+																								GroupLayout.PREFERRED_SIZE,
+																								110,
+																								GroupLayout.PREFERRED_SIZE)
+																						.addComponent(
+																								textField_1,
+																								GroupLayout.PREFERRED_SIZE,
+																								110,
+																								GroupLayout.PREFERRED_SIZE)
+																						.addComponent(
+																								textField_2,
+																								GroupLayout.PREFERRED_SIZE,
+																								110,
+																								GroupLayout.PREFERRED_SIZE)
+																						.addGroup(
+																								gl_contentPane
+																										.createSequentialGroup()
+																										.addGap(19)
+																										.addComponent(
+																												lblNotas,
+																												GroupLayout.PREFERRED_SIZE,
+																												110,
+																												GroupLayout.PREFERRED_SIZE))
+																						.addGroup(
+																								gl_contentPane
+																										.createSequentialGroup()
+																										.addGap(10)
+																										.addComponent(
+																												lblTipodeequipo,
+																												GroupLayout.PREFERRED_SIZE,
+																												110,
+																												GroupLayout.PREFERRED_SIZE))
+																						.addGroup(
+																								gl_contentPane
+																										.createSequentialGroup()
+																										.addGap(10)
+																										.addComponent(
+																												lblCodequipo,
+																												GroupLayout.PREFERRED_SIZE,
+																												110,
+																												GroupLayout.PREFERRED_SIZE))
+																						.addGroup(
+																								gl_contentPane
+																										.createSequentialGroup()
+																										.addGap(10)
+																										.addComponent(
+																												lblPiezareutilizable,
+																												GroupLayout.PREFERRED_SIZE,
+																												110,
+																												GroupLayout.PREFERRED_SIZE))
+																						.addComponent(
+																								textField_4,
+																								GroupLayout.PREFERRED_SIZE,
+																								110,
+																								GroupLayout.PREFERRED_SIZE)
+																						.addGroup(
+																								gl_contentPane
+																										.createParallelGroup(
+																												Alignment.LEADING,
+																												false)
+																										.addGroup(
+																												gl_contentPane
+																														.createSequentialGroup()
+																														.addGap(10)
+																														.addComponent(
+																																lblCausa,
+																																GroupLayout.DEFAULT_SIZE,
+																																GroupLayout.DEFAULT_SIZE,
+																																Short.MAX_VALUE))
+																										.addComponent(
+																												textField_3,
+																												GroupLayout.PREFERRED_SIZE,
+																												110,
+																												GroupLayout.PREFERRED_SIZE)))
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED,
+																				28,
+																				Short.MAX_VALUE)
+																		.addComponent(
+																				scrollPane,
+																				GroupLayout.PREFERRED_SIZE,
+																				593,
+																				GroupLayout.PREFERRED_SIZE))
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addComponent(
+																				toolBar,
+																				GroupLayout.PREFERRED_SIZE,
+																				288,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addContainerGap(
+																				495,
+																				Short.MAX_VALUE)))));
+		gl_contentPane
+				.setVerticalGroup(gl_contentPane
+						.createParallelGroup(Alignment.TRAILING)
+						.addGroup(
+								gl_contentPane
+										.createSequentialGroup()
+										.addComponent(toolBar,
+												GroupLayout.PREFERRED_SIZE, 30,
+												GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(
+												ComponentPlacement.RELATED, 27,
+												Short.MAX_VALUE)
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.BASELINE)
+														.addComponent(
+																scrollPane,
+																GroupLayout.PREFERRED_SIZE,
+																367,
+																GroupLayout.PREFERRED_SIZE)
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addGroup(
+																				gl_contentPane
+																						.createParallelGroup(
+																								Alignment.LEADING)
+																						.addGroup(
+																								gl_contentPane
+																										.createSequentialGroup()
+																										.addGap(27)
+																										.addComponent(
+																												textField,
+																												GroupLayout.PREFERRED_SIZE,
+																												GroupLayout.DEFAULT_SIZE,
+																												GroupLayout.PREFERRED_SIZE)
+																										.addGap(12)
+																										.addComponent(
+																												lblNotas,
+																												GroupLayout.PREFERRED_SIZE,
+																												23,
+																												GroupLayout.PREFERRED_SIZE)
+																										.addGap(5)
+																										.addComponent(
+																												textField_1,
+																												GroupLayout.PREFERRED_SIZE,
+																												GroupLayout.DEFAULT_SIZE,
+																												GroupLayout.PREFERRED_SIZE))
+																						.addComponent(
+																								lblTipodeequipo,
+																								GroupLayout.PREFERRED_SIZE,
+																								23,
+																								GroupLayout.PREFERRED_SIZE))
+																		.addGap(8)
+																		.addComponent(
+																				lblCodequipo,
+																				GroupLayout.PREFERRED_SIZE,
+																				23,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addGap(3)
+																		.addComponent(
+																				textField_2,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addGap(14)
+																		.addComponent(
+																				lblCausa,
+																				GroupLayout.PREFERRED_SIZE,
+																				23,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addGap(1)
+																		.addComponent(
+																				textField_3,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addGap(4)
+																		.addComponent(
+																				lblPiezareutilizable,
+																				GroupLayout.PREFERRED_SIZE,
+																				23,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addComponent(
+																				textField_4,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED,
+																				66,
+																				Short.MAX_VALUE)
+																		.addGroup(
+																				gl_contentPane
+																						.createParallelGroup(
+																								Alignment.BASELINE)
+																						.addComponent(
+																								button_2)
+																						.addComponent(
+																								button_4))))
+										.addContainerGap()));
+
+		btnonEquipo = new JButton("Equipos");
+		toolBar.add(btnonEquipo);
+		btnonEquipo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				Equipos = new TbEquipos();
+				Equipos.setVisible(true);
+			}
+		});
+
+		btnHistorial = new JButton("Historial");
+		toolBar.add(btnHistorial);
+		btnHistorial.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg1) {
+				setVisible(false);
+				Historial = new TbHistorial();
+				Historial.setVisible(true);
+			}
+		});
+
+		btnPrestamos = new JButton("Prestamos");
+		toolBar.add(btnPrestamos);
+		btnPrestamos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg2) {
+				setVisible(false);
+				Prestamos = new TbPrestamos();
+				Prestamos.setVisible(true);
+
+			}
+		});
+
+		btnUsuario = new JButton("Usuarios");
+		btnUsuario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnUsuario.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg3) {
+						setVisible(false);
+						Users = new TbUsers();
+						Users.setVisible(true);
+
+					}
+				});
+
+			}
+		});
+		toolBar.add(btnUsuario);
+
+		table = new JTable();
+		table.setModel(new DefaultTableModel(new Object[][] {
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null }, }, new String[] {
+				"TipoDeEquipo", "Notas", "Cod_Equipo", "Causa",
+				"PiezaReutilizable" }));
+		scrollPane.setViewportView(table);
+		contentPane.setLayout(gl_contentPane);
 	}
 
+	public void setEquipo(TbEquipos Equipos) {
+		this.Equipos = Equipos;
+
+	}
+
+	public void setHistorial(TbHistorial Historial) {
+		this.Historial = Historial;
+
+	}
+
+	public void setPrestamos(TbPrestamos Prestamos) {
+		this.Prestamos = Prestamos;
+	}
+
+	public void setUsers(TbUsers Users) {
+		this.Users = Users;
+	}
 }
--- ProyectoINTEGRADOR/src/TbPrestamos.java
+++ ProyectoINTEGRADOR/src/TbPrestamos.java
@@ -6,10 +6,40 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class TbPrestamos extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_2;
+	private JButton btnEquipos;
+	private JButton btnHistorial;
+	private JButton btnAlmacen;
+	private JButton btnUsuarios;
+	private JToolBar toolBar;
+	private JLabel lblResponsableprestamo;
+	private JLabel lblCodequipo;
+	private JLabel lblFechainicioprestamo;
+	private JLabel lblFechafinalprestamo;
+	private JLabel lblDestinoprestamo;
+	private JButton btnAceptar;
+	private JButton btnCancelar;
+	private TbEquipos Equipos;
+	private TbHistorial Historial;
+	private TbAlmacen Almacen;
+	private TbUsers Users;
 
 	/**
 	 * Launch the application.
@@ -38,13 +68,155 @@ public class TbPrestamos extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JButton btnAceptar = new JButton("Aceptar");
-		btnAceptar.setBounds(738, 374, 89, 23);
+
+		btnAceptar = new JButton("Buscar");
+		btnAceptar.setBounds(10, 375, 89, 23);
 		contentPane.add(btnAceptar);
-		
-		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.setBounds(603, 374, 89, 23);
+
+		btnCancelar = new JButton("Quitar filtro\r\n");
+		btnCancelar.setBounds(109, 375, 89, 23);
 		contentPane.add(btnCancelar);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(233, 53, 631, 358);
+		scrollPane.setToolTipText("");
+		contentPane.add(scrollPane);
+
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(new Object[][] {
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null },
+				{ null, null, null, null, null }, }, new String[] {
+				"ResponsablePrestamo", "CodEquipo", "Fecha_InicioPrestamo",
+				"Fecha_FinalPrestamo", "DestinoPrestamo" }));
+
+		toolBar = new JToolBar();
+		toolBar.setBounds(10, 11, 288, 30);
+		contentPane.add(toolBar);
+
+		btnEquipos = new JButton("Equipos");
+		btnEquipos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				Equipos = new TbEquipos();
+				Equipos.setVisible(true);
+
+			}
+		});
+		toolBar.add(btnEquipos);
+
+		btnHistorial = new JButton("Historial");
+		toolBar.add(btnHistorial);
+		btnHistorial.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				Historial = new TbHistorial();
+				Historial.setVisible(true);
+
+			}
+		});
+
+		btnAlmacen = new JButton("Almacen");
+		toolBar.add(btnAlmacen);
+		btnAlmacen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				Almacen = new TbAlmacen();
+				Almacen.setVisible(true);
+
+			}
+		});
+
+		btnUsuarios = new JButton("Usuarios");
+		toolBar.add(btnUsuarios);
+		btnUsuarios.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				Users = new TbUsers();
+				Users.setVisible(true);
+
+			}
+		});
+
+		lblFechainicioprestamo = new JLabel("Fecha de Inicio del Prestamo");
+		lblFechainicioprestamo.setBounds(10, 161, 146, 23);
+		contentPane.add(lblFechainicioprestamo);
+
+		textField = new JTextField();
+		textField.setBounds(10, 186, 110, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+
+		lblFechafinalprestamo = new JLabel("Fecha Final Prestamo");
+		lblFechafinalprestamo.setBounds(10, 223, 110, 23);
+		contentPane.add(lblFechafinalprestamo);
+
+		textField_1 = new JTextField();
+		textField_1.setBounds(10, 257, 110, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+
+		lblResponsableprestamo = new JLabel("Responsable de Prestamo\r\n");
+		lblResponsableprestamo.setBounds(10, 53, 124, 23);
+		contentPane.add(lblResponsableprestamo);
+
+		lblCodequipo = new JLabel("Codigo del Equipo\r\n");
+		lblCodequipo.setBounds(10, 107, 110, 23);
+		contentPane.add(lblCodequipo);
+
+		textField_3 = new JTextField();
+		textField_3.setBounds(10, 130, 110, 20);
+		textField_3.setColumns(10);
+		contentPane.add(textField_3);
+
+		lblDestinoprestamo = new JLabel("Destino Prestamo");
+		lblDestinoprestamo.setBounds(10, 288, 110, 23);
+		contentPane.add(lblDestinoprestamo);
+
+		textField_4 = new JTextField();
+		textField_4.setBounds(10, 314, 110, 20);
+		textField_4.setColumns(10);
+		contentPane.add(textField_4);
+
+		textField_2 = new JTextField();
+		textField_2.setBounds(10, 81, 110, 20);
+		textField_2.setColumns(10);
+		contentPane.add(textField_2);
+	}
+
+	public void setEquipos(TbEquipos Equipos) {
+		this.Equipos = Equipos;
+	}
+
+	public void setHistorial(TbHistorial Historial) {
+		this.Historial = Historial;
+	}
+
+	public void setAlmacen(TbAlmacen Almacen) {
+		this.Almacen = Almacen;
+	}
+
+	public void setUsers(TbUsers Users) {
+		this.Users = Users;
 	}
 }
